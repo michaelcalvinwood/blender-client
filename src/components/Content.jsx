e74f9348c4b5d1aad5c461439c4669beb4d2fb00
Michael Wood <michaelwood33311@icloud.com>
2023-06-05 11:09:34 -0400

    added getting subinfo for google search

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,9 @@
-import { Box, Select, Text } from '@chakra-ui/react'
+import { Box, Input, Select, Stack, Text } from '@chakra-ui/react'
 import React from 'react';
-import { setContentType } from '../store/sliceContent';
+import { setContentType, setContentSubType, setContentInput, resetContentInfo } from '../store/sliceContent';
 import { useDispatch, useSelector } from 'react-redux';
+import { Radio, RadioGroup } from '@chakra-ui/react';
+
 
 const Content = () => {
     const dispatch = useDispatch();
@@ -22,11 +24,27 @@ const Content = () => {
         
     ]
   return (
-    <Box width="100%" display='flex' justifyContent={'space-between'} alignItems={'center'} margin="1rem 0 0 0">
-        <Text width="19rem">What type of content do you want to add?</Text>
-        <Select width="calc(100% - 20rem)" value={content.type} onChange={(e) => dispatch(setContentType({type: e.target.value}))}>
-            {types.map(type => (<option key={type.id} value={type.id} >{type.type}</option>))} 
-        </Select>
+    <Box>
+        <Box width="100%" display='flex' justifyContent={'space-between'} alignItems={'center'} margin="1rem 0 0 0">
+            <Text width="19rem">What type of content do you want to add?</Text>
+            <Select width="calc(100% - 20rem)" value={content.type} 
+                onChange={(e) => {
+                    dispatch(resetContentInfo());
+                    dispatch(setContentType({type: e.target.value}));
+                }}>
+                {types.map(type => (<option key={type.id} value={type.id} >{type.type}</option>))} 
+            </Select>
+        </Box>
+        {content.type === 'google_search' && (<div>
+            <RadioGroup width="calc(100% - 20rem)" display='flex' justifyContent={'center'} margin=".5rem 0 0 20rem" onChange={(e) => { console.log(e); dispatch(setContentSubType({subType: e}))}} value={content.subType ? content.subType : 'news'}>
+            <Stack direction='row'>
+                <Radio value='news'>News</Radio>
+                <Radio value='web'>Web</Radio>
+                <Radio value='video'>Video</Radio>
+            </Stack>
+            </RadioGroup>
+            <Input width="calc(100% - 20rem)" margin=".5rem 0 0 20rem" type='text' placeholder="Search terms" value={content.input ? content.input : ''} onChange={(e) => dispatch(setContentInput({input: e.target.value}))}/>
+        </div>)}
     </Box>
   )
 }
--- src/store/sliceContent.js
+++ src/store/sliceContent.js
@@ -2,15 +2,28 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const sliceContent = createSlice({
     name: 'output',
-    initialState: {type: 'google_search', mix: []},
+    initialState: {type: 'google_search', subType: '', input: null, mix: []},
     reducers: {
         setContentType: (state, action) => {
             state.type = action.payload.type;
             return state;
+        },
+        setContentSubType: (state, action) => {
+            state.subType = action.payload.subType;
+            return state;
+        },
+        setContentInput: (state, action) => {
+            state.input = action.payload.input;
+            return state;
+        },
+        resetContentInfo: (state, action) => {
+            state.subType = '';
+            state.input = null;
+            return state; 
         }
     }
 });
 
-export const { setContentType } = sliceContent.actions;
+export const { setContentType, setContentSubType, setContentInput, resetContentInfo } = sliceContent.actions;
 
 export default sliceContent.reducer;
