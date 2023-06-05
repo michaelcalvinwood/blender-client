351fbf916bc8f9fb3f82bc12c705e1d021eaf94d
Michael Wood <michaelwood33311@icloud.com>
2023-06-05 09:44:14 -0400

    added selecting the type of output

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { Box, Select, Text } from '@chakra-ui/react'
+import { Box, Select, Text } from '@chakra-ui/react';
+import { setOutputType } from '../store/sliceOutput';
 
 const Output = () => {
     const output = useSelector (state => state.output);
+    const dispatch = useDispatch();
+
     const types = [
         {type: 'news article', id:'news'},
         {type: 'blog post', id: 'blog'}
@@ -11,9 +14,9 @@ const Output = () => {
     const defaultType = types[0];
 
   return (
-    <Box>
-        <Text>What do you want to create?</Text>
-        <Select value={output.type}>
+    <Box width="100%" display='flex' justifyContent={'space-between'} alignItems={'center'} margin="1rem 0 0 0">
+        <Text width="19rem">What do you want to create?</Text>
+        <Select width="calc(100% - 20rem)" value={output.type} onChange={(e) => dispatch(setOutputType({type: e.target.value}))}>
             {types.map(type => (<option key={type.id} value={type.id} >{type.type}</option>))} 
         </Select>
     </Box>
