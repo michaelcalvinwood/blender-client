2e22a695e0e633d6dad4f1d88bc835eca364ec28
Michael Wood <michaelwood33311@icloud.com>
2023-06-06 10:07:01 -0400

    added receiving presigned put url for uploading files to s3 bucket

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -1,11 +1,41 @@
 import React, { useCallback } from 'react'
 import { Box } from '@chakra-ui/react'
 import {useDropzone} from 'react-dropzone'
+import axios from 'axios'
+import {v4 as uuidv4} from 'uuid';
 
 const File = () => {
-    const onDrop = useCallback( async acceptedFiles => {
-        console.log('accepted files', acceptedFiles);
 
+    const uploadFile = async (file, url, folder) => {
+      console.log('uploadFile', file, url, folder);
+    }
+
+    const onDrop = useCallback( async acceptedFiles => {
+        const date = new Date();
+        let folder = date.toISOString();
+        folder = folder.substring(0, folder.indexOf('T'));
+
+        acceptedFiles.forEach(async file => {
+          console.log('accepted files', folder, acceptedFiles);
+          let request = {
+            url: `https://query.pymnts.com:6255/presignedUrl`,
+            method: 'post',
+            data: {
+              key: `${folder}/${uuidv4()}-${file.name}`
+            }
+          }
+
+          console.log('request', request);
+          
+          let response;
+
+          try {
+            response = await axios(request);
+            await uploadFile (file, response.data, folder);
+          } catch (err) {
+            console.error(err);
+          }
+        });
         // foreach file
             // get presigned url
             // upload to dropzone
