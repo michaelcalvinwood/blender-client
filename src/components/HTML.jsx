4529747c9eca19cb73a33754d53f6717d1f202ba
Michael Wood <michaelwood33311@icloud.com>
2023-06-13 09:40:07 -0400

    added displaying HTML options

--- src/components/HTML.jsx
+++ src/components/HTML.jsx
@@ -4,15 +4,23 @@ import { Box, Select, Text } from '@chakra-ui/react';
 import { setOutputLength } from '../store/sliceOutput';
 import { setContentStage } from '../store/sliceContent';
 import { setAlertMsg } from '../store/sliceAlert';
-
+import { Checkbox, CheckboxGroup, Stack } from '@chakra-ui/react'
+import { setHtmBold, setHtmlBullets, setHtmlHeadings, setHtmlLinks, setHtmlTables } from '../store/sliceHTML';
 
 const HTML = () => {
+    const html = useSelector(state => state.html);
+    const dispatch = useDispatch();
+
   return (
     <Box width="100%" display='flex' justifyContent={'space-between'} alignItems={'center'} margin="1rem 0 0 0">
         <Text width="19rem">What HTML elements do you want?</Text>
-        <Select width="calc(100% - 20rem)" value={output.length} onChange={(e) => dispatch(setOutputLength({type: e.target.value}))}>
-            {types.map(type => (<option key={type.id} value={type.id} >{type.type}</option>))} 
-        </Select>
+        <Box display='flex' justifyContent={'space-between'} width="calc(100% - 20rem)">
+            <Checkbox isChecked={html.links} onChange={() => dispatch(setHtmlLinks({links: !html.links}))}>Links</Checkbox>
+            <Checkbox isChecked={html.headings} onChange={() => dispatch(setHtmlHeadings({headings: !html.headings}))}>Headings</Checkbox>
+            <Checkbox isChecked={html.tables} onChange={() => dispatch(setHtmlTables({tables: !html.tables}))}>Tables</Checkbox>
+            <Checkbox isChecked={html.bullets} onChange={() => dispatch(setHtmlBullets({bullets: !html.bullets}))}>Bullets</Checkbox>
+            <Checkbox isChecked={html.bold} onChange={() => dispatch(setHtmBold({bold: !html.bold}))}>Bold</Checkbox>
+        </Box>
     </Box>
   )
 }
